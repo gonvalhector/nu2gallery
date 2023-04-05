22d988b76ddebf68459c3d6e4c84506259f675e0
Hector Gonzalez <gonvalhector@gmail.com>
2023-04-05 13:09:25 -0700

    Updated classes.py

--- classes.py
+++ classes.py
@@ -4,11 +4,49 @@
     - A link to a PNG version of the image
     - A link to a JPG version of the image                            '''
 class Image:
+    # Initializer
     def __init__(self, label="", caption="", link_full="", /
-                link_GIF=None, link_PNG="", link_JPG=""):
-        self.label = label
-        self.caption = caption
-        self.link_full = link_full
-        self.link_GIF = link_GIF
-        self.link_PNG = link_PNG
-        self.link_JPG = link_JPG
+                link_gif=None, link_png="", link_jpg=""):
+        self.__label = label
+        self.__caption = caption
+        self.__link_full = link_full
+        self.__link_gif = link_gif
+        self.__link_png = link_png
+        self.__link_jpg = link_jpg
+
+    # Getters and setters
+    def get_label(self):
+        return self.__label
+    
+    def set_label(self, label):
+        self.__label = label
+
+    def get_caption(self):
+        return self.__caption
+    
+    def set_caption(self, caption):
+        self.__caption = caption
+
+    def get_link_full(self):
+        return self.__link_full
+    
+    def set_link_full(self, link):
+        self.__link_full = link
+
+    def get_link_gif(self):
+        return self.__link_gif
+    
+    def set_link_gif(self, link):
+        self.__link_gif = link
+
+    def get_link_png(self):
+        return self.__link_png
+    
+    def set_link_png(self, link):
+        self.__link_png = link
+
+    def get_link_jpg(self):
+        return self.__link_jpg
+    
+    def set_link_jpg(self, link):
+        self.__link_jpg = link
